0ac581884c0f74c8b46ba949a23b8ff7a6325306
Maximilian Schattauer <max@schattauer.info>
2025-01-26 01:30:14 +0000

    added handler response body

--- lib/api/investments.go
+++ lib/api/investments.go
@@ -3,4 +3,5 @@ package api
 import "net/http"
 
 func GeneralPresentValueHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Write([]byte("{\"presentValue\":3000.0}"))
 }
--- tests/api/investments_test.go
+++ tests/api/investments_test.go
@@ -3,20 +3,21 @@ package api
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	a "xiasitao.de/asset-pricing/lib/api"
 )
 
-func requestResponseWriterFactory() (request *http.Request, responseWriter *httptest.ResponseRecorder) {
-	request, _ = http.NewRequest(http.MethodGet, "/", nil)
+func requestResponseWriterFactory(requestBody string) (request *http.Request, responseWriter *httptest.ResponseRecorder) {
+	request, _ = http.NewRequest(http.MethodGet, "/", strings.NewReader(requestBody))
 	responseWriter = httptest.NewRecorder()
 	return
 }
 
 func TestPresentValue(t *testing.T) {
 	t.Run("test response code", func(t *testing.T) {
-		request, responseWriter := requestResponseWriterFactory()
+		request, responseWriter := requestResponseWriterFactory("")
 		a.GeneralPresentValueHandler(responseWriter, request)
 
 		got := responseWriter.Code
@@ -24,6 +25,18 @@ func TestPresentValue(t *testing.T) {
 
 		if got != expected {
 			t.Errorf("got %d, expected %d", got, expected)
+		}
+	})
+
+	t.Run("test response body", func(t *testing.T) {
+		request, responseWriter := requestResponseWriterFactory("{\"periods\": [{\"cashflow\": 3000.0}]}")
+		a.GeneralPresentValueHandler(responseWriter, request)
+
+		got := responseWriter.Body.String()
+		expected := "{\"presentValue\":3000.0}"
+
+		if got != expected {
+			t.Errorf("got %s, expected %s", got, expected)
 		}
 	})
 
