831bee2b96f7398cdb4fd8bc35b57ac909f51cf6
Maximilian Schattauer <max@schattauer.info>
2025-01-26 02:04:33 +0000

    added http server, added present value handler as main handler

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"net/http"
+
+	"xiasitao.de/asset-pricing/lib/api"
+)
+
+func main() {
+	http.ListenAndServe(":8000", http.HandlerFunc(api.GeneralPresentValueHandler))
+}
