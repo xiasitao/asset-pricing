f9bbb9f9fbfef807416dfa17e0a41beb13acf296
Maximilian Schattauer <max@schattauer.info>
2025-01-26 12:25:20 +0000

    added Azure Functions deployment boilerplate, adapted main to server in Azure Functions

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ build:
 install-azure-tools-apt:
 	@sh deployment/azure-scripts/install_azure_function_tools_apt.sh
 
-.PHONY: build
+.PHONY: build install-azure-tools-apt
--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"xiasitao.de/asset-pricing/lib/api"
 )
 
 func main() {
-	http.ListenAndServe(":8000", &api.ProductionInvestmentsServer)
+	http.ListenAndServe(getListenAddress(), &api.ProductionInvestmentsServer)
+}
+
+func getListenAddress() string {
+	listenAddress := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddress = ":" + val
+	}
+	return listenAddress
 }
