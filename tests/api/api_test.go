c97a73fa0dcf26708f3bb41c1c79350e9a4ed8c1
Max Schattauer <max.schattauer@perelyn.com>
2025-01-31 21:30:09 +0000

    added global server

--- tests/api/api_test.go
+++ tests/api/api_test.go
@@ -1,1 +1,30 @@
-package api_test
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"xiasitao.de/asset-pricing/lib/api"
+)
+
+type SpyInvestmentsServer struct {
+	callCount int
+}
+
+func (s *SpyInvestmentsServer) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	s.callCount++
+}
+
+func TestServer(t *testing.T) {
+	mockRequest := httptest.NewRequest(http.MethodPost, "/general-present-value", nil)
+	mockResponse := httptest.NewRecorder()
+
+	mockInvestmentsServer := SpyInvestmentsServer{}
+	server := api.NewAssetPricingServer(&mockInvestmentsServer)
+	server.ServeHTTP(mockResponse, mockRequest)
+
+	if mockInvestmentsServer.callCount != 1 {
+		t.Errorf("expected call, didn't receive one")
+	}
+}
